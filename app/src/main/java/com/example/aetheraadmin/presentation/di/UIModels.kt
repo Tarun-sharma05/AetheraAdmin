642fca416bee3fa67c13709d223320fccf2efb5c
Tarun Sharma <tarundadhich05@gmail.com>
2026-03-31 23:41:51 +0530

    Added FirebaStorage parameter in UIModels.kt

--- app/src/main/java/com/example/aetheraadmin/presentation/di/UIModels.kt
+++ app/src/main/java/com/example/aetheraadmin/presentation/di/UIModels.kt
@@ -3,6 +3,7 @@ package com.example.aetheraadmin.presentation.di
 import com.example.aetheraadmin.data.repoimpl.repoImpal
 import com.example.aetheraadmin.domain.repo.repo
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,10 +16,13 @@ object UIModels {
 
     @Provides
     fun provideRepo(
-        firestore: FirebaseFirestore
+        firestore: FirebaseFirestore,
+        storage: FirebaseStorage
     ): repo{
         return repoImpal(
-            firestore
+            FirebaseFirestore = firestore,
+            FirebaseStorage = storage
+
         )
     }
 }
